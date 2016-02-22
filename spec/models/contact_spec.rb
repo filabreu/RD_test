a33c8c62e38cb716c37a71964649f8b3a5e2e51f
Filipe Abreu <filabreu@gmail.com>
2016-02-22 12:27:53 -0300

    Add spec for size of states enum in Contact model.

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Contact, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  context "states" do
+    it "should have 27 options" do
+      expect(Contact.states.size).to eq(27)
+    end
+  end
 end
